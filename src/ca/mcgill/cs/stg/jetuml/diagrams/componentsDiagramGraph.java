5ef596d2d7448b68ab0166dc8921e03acf07d3c5
12111312 <12111312@W1975176.portoalegre.pucrsnet.br>
2017-06-13 22:12:54 -0300

    #7

---
+++ src/ca/mcgill/cs/stg/jetuml/diagrams/componentsDiagramGraph.java
@@ -0,0 +1,33 @@
+package ca.mcgill.cs.stg.jetuml.diagrams;
+
+import ca.mcgill.cs.stg.jetuml.graph.Edge;
+import ca.mcgill.cs.stg.jetuml.graph.Graph;
+import ca.mcgill.cs.stg.jetuml.graph.Node;
+
+public class componentsDiagramGraph extends Graph {
+
+	@Override
+	public String getFileExtension() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Node[] getNodePrototypes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Edge[] getEdgePrototypes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
